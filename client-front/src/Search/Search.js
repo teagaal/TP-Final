723d6a3d3a7b109ca0737e865fb9ffcb617a0a1f
teagaal <smaurenza@disroot.org>
2018-07-23 19:24:12 -0300

    Corregí el Router

--- client-front/src/Search/Search.js
+++ client-front/src/Search/Search.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class Search extends Component {
   constructor(props) {
